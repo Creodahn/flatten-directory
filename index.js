8e48daf267fc67d1607023aea35f92ae2536caa8
Justin Drew <jdrew@sca-corp.com>
2016-11-21 11:34:00 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,90 @@
+
+const fs = require('fs'),
+      mv = require('mv'),
+      path = require('path');
+
+const args = process.argv.slice(2),
+      log = console.log,
+      overwrite = Boolean(args[2]),
+      sourceDir = args[0],
+      targetDir = args[1];
+
+parseDirectory(sourceDir);
+
+function checkFileExistence(dir, fileName) {
+  //assume the file exists until proven wrong
+  var result = true;
+
+  try {
+    fs.statSync(path.resolve (dir, fileName));
+  } catch(err) {
+    //if fs.statSync errors, the file doesn't exist
+    result = false;
+  }
+
+  return result;
+}
+
+function parseDirectory(dir) {
+  fs.readdir(dir, function(err, list) {
+    for(let i = 0; i < list.length; i++) {
+      let item = list[i],
+          location = path.resolve(dir, item);
+
+      log(item);
+
+      if(fs.lstatSync(location).isDirectory()) {
+        parseDirectory(location);
+      } else {
+        let name = item;
+
+        if(!overwrite) {
+          if(checkFileExistence(targetDir, name)) {
+            name = renameFile(name);
+          }
+        }
+
+        mv(location, path.resolve(targetDir, name), { mkdirp: true }, function(err) {
+          if(err) {
+            console.error(err);
+          }
+        });
+      }
+    }
+  });
+}
+
+function removeExtension(name) {
+  let eman = reverse(name),
+      ext = eman.substring(0, eman.indexOf('.'));
+
+  return {
+    name: reverse(eman.substring(eman.indexOf('.') + 1)),
+    extension: reverse(ext)
+  };
+}
+
+function renameFile(name) {
+  let alteredName = removeExtension(name),
+      ext = alteredName.extension,
+      nameOnly = alteredName.name,
+      num = 0,
+      numString = '',
+      result = '';
+
+  while(checkFileExistence(targetDir, nameOnly.concat(numString, '.', ext))) {
+    if(num === 0) {
+      numString = '';
+    } else {
+      numString = ' (' + num.toString () + ')';
+    }
+
+    num++;
+  }
+
+  return nameOnly.concat(numString, '.', ext);
+}
+
+function reverse(str) {
+  return str.split ('').reverse ().join ('');
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+* Flatten Directory structure
+
+This script allows you to move all files in a directory structure into a single target directory.
+
+```node index.js <source_directory> <target_directory>```
